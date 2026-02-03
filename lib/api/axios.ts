6062efbb86c44a8657dd9b208578eb7522328322
anjal <anjalmani2023@gmail.com>
2026-02-03 17:35:22 +0545

    aligned with folder api and auth

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -1,13 +1,17 @@
 import axios from "axios";
 
-const BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000";
-
-const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+const api = axios.create({
+  baseURL: "http://localhost:4000/api",
 });
 
-export default axiosInstance;
+api.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
+
+export default api;
